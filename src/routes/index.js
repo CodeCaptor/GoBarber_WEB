02fe08fc5e5ffa0e533c289da30653ca7ef8cc52
KZTN <kakaiocesar2@gmail.com>
2020-07-20 16:15:43 -0300

    :art: Added customized Route component
    This feature comes to control private routes and evoid to be shown to unregisted users to access

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter, Switch } from 'react-router-dom';
+import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
@@ -8,11 +8,15 @@ import Profile from '../pages/Profile';
 
 export default function Routes() {
   return (
-    <Switch>
-      <Route path="/" exact component={SignIn} />
-      <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={SignIn} />
+        <Route path="/register" component={SignUp} />
+
+        <Route path="/dashboard" component={Dashboard} isPrivate />
+        <Route path="/profile" component={Profile} isPrivate />
+        <Route path="/" component={() => <h1>404</h1>} />
+      </Switch>
+    </BrowserRouter>
   );
 }
