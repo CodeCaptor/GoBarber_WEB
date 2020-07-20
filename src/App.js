e3358cdbc84d3fb7756717ff40f6c2ecf5536465
KZTN <kakaiocesar2@gmail.com>
2020-07-20 20:52:11 -0300

    :building_construction: Added Redux architectural structure
    Using Redux, React-redux & redux saga. All initially integrated & implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import Routes from './routes';
-import history from './services/history';
 import './config/ReactotronConfig';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Routes from './routes';
+import store from './store';
+import history from './services/history';
 import GLobalStyle from './styles/global';
 
 function App() {
     return (
         <div className="App">
-            <Router history={history}>
-                <Routes />
-                <GLobalStyle />
-            </Router>
+            <Provider store={store}>
+                <Router history={history}>
+                    <Routes />
+                    <GLobalStyle />
+                </Router>
+            </Provider>
         </div>
     );
 }
---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,8 @@
+import { INITIAL_STATE } from './types';
+
+export default function auth(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
