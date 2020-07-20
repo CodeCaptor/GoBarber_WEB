78fb3fcd723fbf666e38d9c634d9792ef8950702
KZTN <kakaiocesar2@gmail.com>
2020-07-20 17:10:02 -0300

    :art: Created GlobalStyle for entire app
    all changes are functional

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 import './config/ReactotronConfig';
+import GLobalStyle from './styles/global';
 
 function App() {
   return (
     <div className="App">
       <Router history={history}>
         <Routes />
+        <GLobalStyle />
       </Router>
     </div>
   );
