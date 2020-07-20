06f5150b747e60c9a28db537dd377409180f7901
KZTN <kakaiocesar2@gmail.com>
2020-07-20 15:37:25 -0300

    :tada: Initial commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from './routes';
+import history from './services/history';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
